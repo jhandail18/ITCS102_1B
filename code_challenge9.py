8e225a21e477b9389c8e148b529741a2f651702d
JD_RTX3050 <jhandailfuruc@gmail.com>
2025-09-19 06:53:10 +0800

    Add files via upload

---
+++ code_challenge9.py
@@ -0,0 +1,28 @@
+# Create a fun program that acts like a parrot — it repeats anything the user says, using a for loop to "squawk" it back multiple times!
+
+# This challenge focuses purely on:
+# Using a simple for loop
+# Working with strings
+# Repeating an action without storing data
+# Ask the user to enter a phrase (e.g., "I love Python!").Ask how many times they want the parrot to repeat it.Use a for loop to print the phrase that many times.
+# Make each line sound like a parrot: " Squawk! {phrase}"   
+
+# Copy this bitmap image -> 
+
+# 🦜
+
+# Expected output 🦜 PARROT SIMULATOR – THE ECHO CHAMBER!What do you want your parrot to say? I am hungry!
+# How many times should the parrot squawk it? 4
+
+# Listen to your parrot:
+# 🦜 Squawk! I am hungry!
+# 🦜 Squawk! I am hungry!
+# 🦜 Squawk! I am hungry!
+# 🦜 Squawk! I am hungry!
+
+
+text = str(input("What do you want your pet to say?--> "))
+loop = eval(input("How many times should your pet to squawk it? --> "))
+
+for i in range(loop, 0, -1):
+    print("🦜 Squawk! Squawk!",text,"!")
