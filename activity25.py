032679484bfd1e2fdc6af13aa45010118e7efeb0
JD_RTX3050 <jhandailfuruc@gmail.com>
2025-11-20 23:06:25 +0800

    Add files via upload

---
+++ activity25.py
@@ -0,0 +1,19 @@
+
+#DICTIONARY
+programming_language = ['Java','Javascript','Python','C#','C++','PHP']
+
+#index
+print(programming_language[-1])
+
+# {KEYS : VALUES, KEYS: VALUES}
+
+programming_language_dictionary = {'Level 1':'Python','Level 2':'Java','Level 3':'C#','Level 4':'Perl','Level 5':'Javascript'}
+
+print(programming_language_dictionary['Level 3'])
+
+#ADDING ITEM TO A DICTIONARY
+
+programming_language_dictionary['Level 1'] = 'html'
+print(programming_language_dictionary)
+
+programming_language_dictionary.pop()
