1c1d9f5c6a27c59e7c1121aa639253feda8311d3
JD_RTX3050 <jhandailfuruc@gmail.com>
2025-11-20 23:05:27 +0800

    Add files via upload

---
+++ activity23.py
@@ -0,0 +1,21 @@
+fruit1 = 'Apples'
+fruit2 = 'Mangoes'
+
+fruits = ['Apple','Mango','Atis','Orange','Aratilis','Duhat']
+print(fruits)
+print(fruits[2:5])
+
+fruits.append("Pineapple")
+print(fruits)
+
+fruits.pop()
+print(fruits)
+
+fruits.remove("Atis")
+print(fruits)
+
+fruits.insert(2, "Grapes")
+print(fruits)
+
+fruits.sort()
+print(fruits)
