81c0d11996ec706944f03646fdd059144c2f31d3
JD_RTX3050 <jhandailfuruc@gmail.com>
2025-12-01 01:32:13 +0800

    Add files via upload

---
+++ code_challenge14.py
@@ -0,0 +1,22 @@
+step = True
+trial = 0
+
+while step is True:
+    dungeon = input("Would you like to walk into Dungeon?: (y/n) ")
+    if dungeon == 'yes':
+        print("You Entered to Dungeon")
+        walk = input("Continue?: (y/n)")
+        if walk == "y":
+            trial += 1
+            print("Walk deeper..")
+            continue
+        elif walk == 'n':
+            print(f"You leaved the Dungeon.. You walked {trial} steps")
+            break
+        elif dungeon == 'n':
+            print("Got scared and run away...")
+            print(f"number of steps, {trial}")
+            break
+        
+    else:
+        print("Invalid Input.")
